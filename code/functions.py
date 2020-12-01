5c7efa975f2aafe2126a326cf081ab922369a75d
Cristina Sahoo <s_cristina2011@yahoo.com>
2020-11-30 23:01:46 -0600

    classification models

--- code/functions.py
+++ code/functions.py
@@ -1,4 +1,6 @@
 import string
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix
 
 def to_snake_case(name):
     if name == name.upper():
@@ -10,3 +12,32 @@ def to_snake_case(name):
         if letter in string.ascii_uppercase:
             name = name.replace(letter, '_' + letter.lower())
     return name
+
+def get_scores(gs, X_train, y_train, X_test, y_test):
+    best_score = gs.best_score_
+    train_score = gs.score(X_train, y_train)
+    test_score = gs.score(X_test, y_test)
+    preds = gs.predict(X_test)
+    tn, fp, fn, tp = confusion_matrix(y_test, preds).ravel()
+    # Sensitivity
+    sensitivity = tp / (tp+fn)
+    # Specificity
+    specificity = tn / (tn+fp)
+    # Precision
+    precision = tp / (tp+fp)
+    precision = metrics.precision_score(y_test, preds)
+    # Accuracy
+    accuracy = (tp + tn) / (tn + fp + fn + tp)
+    accuracy = metrics.accuracy_score(y_test, preds)
+    f1_score = metrics.f1_score(y_test, preds)
+    scores_list = [
+               round(best_score, 4), 
+               round(train_score, 4), 
+               round(test_score, 4), 
+               round(sensitivity, 4), 
+               round(specificity, 4), 
+               round(precision, 4), 
+               round(accuracy, 4),
+               round(f1_score, 4)
+              ]
+    return scores_list
